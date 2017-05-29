1434c23feca8fa7280f40df92015fb642ad49f90
Bowen Feng <fengbowen@i.shu.edu.cn>
2017-05-29 13:37:12 +0800

    20170526

--- sina_stock_market.py
+++ sina_stock_market.py
@@ -17,6 +17,7 @@ F3 = ""
 F4 = ""
 F5 = ""
 F6 = ""
+Heading = "股票代码,股票代码乱,今日开盘价,昨日收盘价,当前价格,今日最高价,今日最低价,买一报价,卖一报价,成交数量,成交金额,买一数量,买一报价,买二数量,买二报价,买三数量,买三报价,买四数量,买四报价,买五数量,买五报价,卖一数量,卖一报价,卖二数量,卖二报价,卖三数量,卖三报价,卖四数量,卖四报价,卖五数量,卖五报价,日期,时间"
 _session = requests.session()
 
 # 000001-000009
@@ -28,7 +29,7 @@ for i in range(1,10):
     if len(stock) > 11:
         F11 = stock[-1]
         F12 = stock[0:6] + ","
-        F13 = stock[8:-3]
+        F13 = stock[8:-6]
         F1 += F11 + F12 + F13
     else:
         pass
@@ -42,7 +43,7 @@ for i in range(10,100):
 	if len(stock) > 11:
 		F21 = stock[-1]
 		F22 = stock[0:6] + ","
-		F23 = stock[8:-3]
+		F23 = stock[8:-6]
 		F2 += F21 + F22 + F23
 	else:
 		pass
@@ -56,7 +57,7 @@ for i in range(100,1000):
 	if len(stock) > 11:
 		F31 = stock[-1]
 		F32 = stock[0:6] + ","
-		F33 = stock[8:-3]
+		F33 = stock[8:-6]
 		F3 += F31 + F32 + F33
 	else:
 		pass
@@ -70,7 +71,7 @@ for i in range(1000,3000):
 	if len(stock) > 11:
 		F41 = stock[-1]
 		F42 = stock[0:6] + ","
-		F43 = stock[8:-3]
+		F43 = stock[8:-6]
 		F4 += F41 + F42 + F43
 	else:
 		pass
@@ -84,7 +85,7 @@ for i in range(300001,301000):
 	if len(stock) > 11:
 		F51 = stock[-1]
 		F52 = stock[0:6] + ","
-		F53 = stock[8:-3]
+		F53 = stock[8:-6]
 		F5 += F51 + F52 + F53
 	else:
 		pass
@@ -98,10 +99,11 @@ for i in range(600001,605000):
 	if len(stock) > 11:
 		F61 = stock[-1]
 		F62 = stock[0:6] + ","
-		F63 = stock[8:-3]
+		F63 = stock[8:-6]
 		F6 += F61 + F62 + F63
 	else:
 		pass
 
 F = F1 + F2 + F3 + F4 + F5 + F6
+print(Heading)
 print(F)
